7ef0036d04acc36c2321b16bc9561ed78e8e7dc7
sboursault <sebastien.boursault@gmail.com>
2017-04-19 15:59:43 +0200

    add documentation

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # gherkin-htmlify
-Use node to generate html pages from your feature files and share them easier.
+
+Generate html pages from your feature files and share them easier.
+
+## examples of generated pages
+
+- Overview:
+
+![Alt Text](./doc/atm-doc-overview.png)
+
+- Feature:
+
+![Alt Text](./doc/atm-doc-pinRecognition.png)
+
+## how to run gherkin-htmlify
+
+  Create a file named runner.js with content like:
+
+    var gherkinHtmlify = require('./gherkin-htmlify');
+    var featureDirectoryPath = '<your feature file folder>';
+    var outputDirectory = '<where to put the html files>';
+    var options = {
+      mainTitle: "<title to use>"
+    };                                                                     
+    gherkinHtmlify.process(featureDirectoryPath, outputDirectory, options);
+  
+  Now, checkout the gherkin-htmlify project to generate html pages
+  
+    git clone https://github.com/sboursault/gherkin-htmlify.git
+    npm install file ncp
+    node runner.js
+
+   
+
+    
+   
--- index.js
+++ index.js
@@ -1,83 +1,7 @@
 
 var fs = require("fs");
 var file = require("file");
-var path = require('path');
-
-function process(featureDirectoryPath, outputDir, options) {
-  if (fs.existsSync(outputDir)) {
-    throw outputDir + ' already exists. Please remove it manually.';
-  }
-  copyFolderRecursiveSync(__dirname + '/html', outputDir);
-
-  var features = [];
-
-  file.walkSync(featureDirectoryToScan,
-    function callback(directory, dirs, files) {
-
-      files.forEach(function(file) {
-
-        var filePath = directory + '/' + file;
-        var fileContent = fs.readFileSync(filePath, 'utf8');
-
-        console.log(filePath);
-
-        var regExp, match;
-        var feature = {};
-        feature.name = '';
-        feature.description = '';
-        feature.meta = []
-        feature.tests = [];
-        feature.path = filePath.replace(featureDirectoryPath, '').replace('.feature', '');
-        features.push(feature);
-
-        var zeroOrMoreAnnotationLine = '(?: *@.*\\n)*';
-        var scenarioHeader = zeroOrMoreAnnotationLine + ' *Scenario\\b.*:.*';
-        var anyNumberOfChararctersButAsFewAsPossible = '[\\s\\S]+?';
-        var followedByAScenarioOrEndOfFile = '(?=(?:' + scenarioHeader + ')|$)';
-
-        match = /([\s\S]*)feature\s*:/gi.exec(fileContent);
-        if (match && match[1].trim()) {
-          feature.meta = match[1].replace(/\s+/g, ' ').trim().split(' ');
-        }
-        match = /feature\s*:\s*(.*)\s*\n/gi.exec(fileContent);
-        if (match) {
-          feature.name = match[1];
-        }
-
-        regExp = new RegExp( 'feature\\s*:.*\\n' + '(' + anyNumberOfChararctersButAsFewAsPossible + ')' + followedByAScenarioOrEndOfFile, 'gi');
-        match = regExp.exec(fileContent);
-        if (match) {
-          feature.description = match[1].trim();
-        }
-
-        regExp = new RegExp( scenarioHeader + anyNumberOfChararctersButAsFewAsPossible + followedByAScenarioOrEndOfFile, 'gi');
-        while((match = regExp.exec(fileContent)) !== null) {
-          var text = match[0];
-          var test = {};
-          test.name = /scenario.*:\s*(.*)/gi.exec(text)[1].trim();
-          console.log('  > ' + test.name);
-
-          match = /([\s\S]*)scenario\s*:/gi.exec(text);
-          if (match && match[1].trim()) {
-            test.meta = match[1].replace(/\s+/g, ' ').trim().split(' ');
-          }
-
-          test.content = /scenario.*:.*\s*\n([\s\S]*)/gi.exec(text)[1];
-          feature.tests.push(test);
-        }
-
-      });
-    });
-
-    var features = JSON.stringify(features, null, "\t");
-
-    var output = "var source = {};\n"
-    output += "source.project = '" + options.mainTitle + "';\n";
-    output += "source.features = " + features + ";\n";
-    output += "source.date = '" + getFormattedDate() + "';\n";
-
-    fs.writeFileSync(outputDir +'/data.js', output, {encoding: 'utf8'});
-}
+var ncp = require('ncp').ncp;
 
 var getFormattedDate = function() {
   var date = new Date();
@@ -93,37 +17,90 @@ var getFormattedDate = function() {
   var day  = date.getDate();
   day = (day < 10 ? "0" : "") + day;
   return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
-}
+};
 
 
-function copyFileSync( source, target ) {
-  var targetFile = target;
-  //if target is a directory a new file with the same name will be created
-  if ( fs.existsSync( target ) ) {
-    if ( fs.lstatSync( target ).isDirectory() ) {
-      targetFile = path.join( target, path.basename( source ) );
+var gherkinHtmlify = {
+  process: function(featureDirectoryPath, outputDir, options) {
+    if (fs.existsSync(outputDir)) {
+      throw outputDir + ' already exists. Please remove it manually.';
     }
-  }
-  fs.writeFileSync(targetFile, fs.readFileSync(source));
-}
+    fs.mkdirSync(outputDir);
+    ncp(__dirname + '/html', outputDir);
 
-function copyFolderRecursiveSync( source, target ) {
-  var files = [];
-  //check if folder needs to be created or integrated
-  var targetFolder = path.join( target, path.basename( source ) );
-  if ( !fs.existsSync( targetFolder ) ) {
-      fs.mkdirSync( targetFolder );
+    console.log('Processing feature files...');
+    var features = [];
+
+    file.walkSync(featureDirectoryPath,
+      function callback(directory, dirs, files) {
+
+        files.forEach(function(file) {
+
+          var filePath = directory + '/' + file;
+          var fileContent = fs.readFileSync(filePath, 'utf8');
+
+          console.log(filePath);
+
+          var regExp, match;
+          var feature = {};
+          feature.name = '';
+          feature.description = '';
+          feature.meta = []
+          feature.tests = [];
+          feature.path = filePath.replace(featureDirectoryPath, '').replace('.feature', '');
+          features.push(feature);
+
+          var zeroOrMoreAnnotationLine = '(?: *@.*\\n)*';
+          var scenarioHeader = zeroOrMoreAnnotationLine + ' *Scenario\\b.*:.*';
+          var anyNumberOfChararctersButAsFewAsPossible = '[\\s\\S]+?';
+          var followedByAScenarioOrEndOfFile = '(?=(?:' + scenarioHeader + ')|$)';
+
+          match = /([\s\S]*)feature\s*:/gi.exec(fileContent);
+          if (match && match[1].trim()) {
+            feature.meta = match[1].replace(/\s+/g, ' ').trim().split(' ');
+          }
+          match = /feature\s*:\s*(.*)\s*\n/gi.exec(fileContent);
+          if (match) {
+            feature.name = match[1];
+          }
+
+          regExp = new RegExp( 'feature\\s*:.*\\n' + '(' + anyNumberOfChararctersButAsFewAsPossible + ')' + followedByAScenarioOrEndOfFile, 'gi');
+          match = regExp.exec(fileContent);
+          if (match) {
+            feature.description = match[1].trim();
+          }
+
+          regExp = new RegExp( scenarioHeader + anyNumberOfChararctersButAsFewAsPossible + followedByAScenarioOrEndOfFile, 'gi');
+          while((match = regExp.exec(fileContent)) !== null) {
+            var text = match[0];
+            var test = {};
+            test.name = /scenario.*:\s*(.*)/gi.exec(text)[1].trim();
+            console.log('  > ' + test.name);
+
+            match = /([\s\S]*)scenario\s*:/gi.exec(text);
+            if (match && match[1].trim()) {
+              test.meta = match[1].replace(/\s+/g, ' ').trim().split(' ');
+            }
+
+            test.content = /scenario.*:.*\s*\n([\s\S]*)/gi.exec(text)[1];
+            feature.tests.push(test);
+          }
+
+        });
+      });
+
+      var features = JSON.stringify(features, null, "\t");
+
+      var output = "var source = {};\n"
+      output += "source.project = '" + options.mainTitle + "';\n";
+      output += "source.features = " + features + ";\n";
+      output += "source.date = '" + getFormattedDate() + "';\n";
+
+      fs.writeFileSync(outputDir +'/data.js', output, {encoding: 'utf8'});
+
+      console.log('Process finished.');
   }
-  //copy
-  if ( fs.lstatSync( source ).isDirectory() ) {
-    files = fs.readdirSync( source );
-    files.forEach( function ( file ) {
-      var curSource = path.join( source, file );
-      if ( fs.lstatSync( curSource ).isDirectory() ) {
-        copyFolderRecursiveSync( curSource, targetFolder );
-      } else {
-        copyFileSync( curSource, targetFolder );
-      }
-    } );
-  }
-}
+
+};
+
+module.exports = gherkinHtmlify;
