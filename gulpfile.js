1a55a0a82f752aa17551c36c355c6ab15ce19d45
sboursault <sebastien.boursault@gmail.com>
2017-05-11 06:54:39 +0200

    add tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
-
+const mochaPhantomJS = require('gulp-mocha-phantomjs');
 /*gulp.task('watch', function() {
    gulp.watch('*.test.js', ['test']);
 });*/
 
-gulp.task('test', () =>
+gulp.task('parser-test', () =>
   gulp.src('*.test.js', {read: false})
     .pipe(mocha())
 );
 
-gulp.task('default', ['test']);
+gulp.task('ui-test', function () {
+    return gulp
+    .src('html/test_runner.html')
+    .pipe(mochaPhantomJS());
+});
+
+gulp.task('default', ['parser-test', 'ui-test']);
